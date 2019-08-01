acafa1af24e6c8bc4371b179d3682d9ccd766162
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-07-31 18:23:10 -0700

    the functionality works now

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//get val 
+
+
+
+$('button').on('click', function(evt){
+   let skill = $('input').val();
+   var template= `
+   <tr>
+   <td><img src="https://i.imgur.com/S44m3ha.png"></td>
+    <td>${skill}</td>
+   </tr>
+   `;
+
+   $('tbody').append(template);
+
+   $('input').val("test");
+
+});
+
+$('tbody').on('click', 'img',function(){
+    $(this).closest('tr').fadeOut(1000, function(){
+        $(this).remove();
+    });
+    })
